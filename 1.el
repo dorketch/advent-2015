9f5ad7f990bf995157ac18166a608284a88824a8
Martin Mailhot <32225011+dorketch@users.noreply.github.com>
2024-07-03 21:43:10 -0400

    Update 1.el
    On ne pouvait pas bien lire le commentaire sur github...

--- 1.el
+++ 1.el
@@ -1,6 +1,7 @@
 ;; Compter les parentheses
 ;; ***********************
-;; On ouvre le fichier dans un buffer temporaire pour ne pas polluer notre buffer principale. Ensuite on utilise la fonction count-matches qui compte le nombre d'occurence d'une expression régulière. On soustrait val1 et val2 puis on obtient le résultat 232.
+;; On ouvre le fichier dans un buffer temporaire pour ne pas polluer notre buffer principale. 
+;; Ensuite on utilise la fonction count-matches qui compte le nombre d'occurence d'une
 
 (with-temp-buffer
   (insert-file-contents "input/1.txt")
